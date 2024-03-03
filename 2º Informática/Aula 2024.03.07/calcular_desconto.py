8260beabdfef22e5ebbfe3d2fb2db408f55569f2
Francisco Coelho <franciscocoelho.ti@gmail.com>
2024-03-03 19:00:38 -0300

    Atualizando arquivos de aula

---
+++ 2º Informática/Aula 2024.03.07/calcular_desconto.py
@@ -0,0 +1,7 @@
+# Construa um algoritmo em Python que receba do usuário o valor de uma determinada venda, e o percentual de desconto.
+# Ao final da execução, exiba o valor que o usuário deve pagar com o desconto.
+
+
+
+
+
