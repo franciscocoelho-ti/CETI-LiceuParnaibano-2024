481ac461aa921e7cde8d273f6b891cdcbb99efb6
Francisco Coelho <franciscocoelho.ti@gmail.com>
2024-03-03 18:36:32 -0300

    Atualizando arquivos de aula

---
+++ 2º Desenvolvimento de Sistemas/Aula 2024.02.26/calcular_desconto.py
@@ -0,0 +1,15 @@
+# Construa um algoritmo em Python que receba do usuário o valor de uma determinada venda, e o percentual de desconto.
+# Ao final da execução, exiba o valor que o usuário deve pagar com o desconto.
+
+
+# Valores de Entrada
+valor_venda = float(input('Informe o valor da venda: '))
+porcentagem_desconto = float(input('Informe a porcentagem de desconto: '))
+
+# Processamento --> O que temos que encontrar?
+valor_desconto = (valor_venda / 100) * porcentagem_desconto
+valor_pagar = valor_venda - valor_desconto
+
+# Informações de saída
+print('O valor a pagar será, ', valor_pagar)
+
