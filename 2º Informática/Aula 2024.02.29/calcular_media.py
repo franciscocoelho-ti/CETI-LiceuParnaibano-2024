f2686c9c6e743049e3ef1725075a1b1592f04faf
Francisco Coelho <franciscocoelho.ti@gmail.com>
2024-03-07 09:21:44 -0300

    Atualizando material de aula

--- 2º Informática/Aula 2024.02.29/calcular_media.py
+++ 2º Informática/Aula 2024.02.29/calcular_media.py
@@ -1,4 +1,5 @@
-# Crie um algoritmo em Python que receba do usuário (aluno) 4 notas, e ao final exiba a média para o usuário.
+# Crie um algoritmo em Python que receba do usuário (aluno) 4 notas, e ao
+# final exiba a média para o usuário.
 
 # Entradas:
 nota1 = float(input('Informe a 1ª nota: '))
@@ -6,12 +7,9 @@ nota2 = float(input('Informe a 2ª nota: '))
 nota3 = float(input('Informe a 3ª nota: '))
 nota4 = float(input('Informe a 4ª nota: '))
 
-
 # Processamento --> Gerar a média
 media = (nota1 + nota2 + nota3 + nota4) / 4
 
-
 # Exibir a saída
 print('A média dos notas foi: ', media)
-
 
--- 2º Informática/Aula 2024.03.07/calcular_desconto.py
+++ 2º Informática/Aula 2024.03.07/calcular_desconto.py
@@ -1,7 +1,23 @@
-# Construa um algoritmo em Python que receba do usuário o valor de uma determinada venda, e o percentual de desconto.
-# Ao final da execução, exiba o valor que o usuário deve pagar com o desconto.
+
+# Escreva um algoritmo que receba do usuário o valor de um produto,
+# e a porcentagem de desconto. Ao final exiba o valor final a ser
+# pago.
 
 
+# Variáveis de Entrada (o que tenho que receber)
+valor_produto = float(input('Informe o valor do produto: '))
+porcentagem_desconto = int(input('Informe a porcentagem do desconto: '))
 
+
+# Processamento (O que a gente deve fazer)
+# 1º - Calcular o valor do desconto
+# 2º - Subtrair o valor do desconto, do valor do produto
+
+valor_desconto = (valor_produto / 100) * porcentagem_desconto
+valor_final = valor_produto - valor_desconto
+
+print('Valor Produto: ', valor_produto)
+print('Desconto: ', valor_desconto)
+print('Valor Final: ', valor_final)
 
 
